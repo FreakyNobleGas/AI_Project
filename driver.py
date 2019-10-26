92096d916a002c86661ff4e1521b7ab208a99d40
Nick Quinn <nickquinn3131@gmail.com>
2019-10-26 13:22:57 -0400

    Added header and stub code

---
+++ driver.py
@@ -0,0 +1,27 @@
+###########################################################################
+###########################################################################
+# Authors: Adrien Forkum, Nick Quinn, Karl
+# Date: 10/26/19
+#
+# Description: This file is the start point for the entire program and contains
+#              all neccessary library imports and driver code to execute the program.
+#
+# Requirements: Python3.5 or higher. PYQT5 graphics library installed.
+#
+# Usage: Python3 driver.py
+###########################################################################
+###########################################################################
+
+# Library Imports ** UNCOMMENT WHEN OTHER FILES ARE READY **
+# from agents import *
+# from algorithms import *
+# from interface import *
+# from maps import *
+
+def main():
+    print("Hello World")
+
+
+
+
+main()
