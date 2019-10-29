25ba9f3fa730f8f9f7d851752e7b9a294a3e43ea
Sasquats <aforkum87@gmail.com>
2019-10-28 21:41:58 -0400

    test commit

--- agents.py
+++ agents.py
@@ -13,11 +13,13 @@ def agents_foo():
     print("In Agents Foo")
     
 class Hunter:
-	def __init__(_current_pos=None, _image=None):
+	def __init__(_current_pos=None, _image=None, _algorithm=None):
 		current_pos = _current_pos
 		image = _image
+		algorithm = _algorithm
 
 class Runner:
-	def __init__(_current_pos=None, _image=None):
+	def __init__(_current_pos=None, _image=None, _algorithm=None):
 		current_pos = _current_pos
 		image = _image
+		algorithm = _algorithm
