05e967129dbf98edb7e926c7e176811526f2c4f7
Karl H <karlh.work@gmail.com>
2019-11-20 20:47:11 -0500

    gameengine.py reworked to use 1x1 tiles: working basic collision check

--- gameengine.py
+++ gameengine.py
@@ -20,15 +20,16 @@ import time
 import os.path
 import pygame
 
-sScale =.4
+sScale =.5
 spriteR = 20*sScale
 #sprite radius.  should be directly related to sprite/grid size
 #currently, all sprites are 40x40, so sR=20
+#this is a scaling factor only used for sprite movement and window scale
 
 
 #gamewindow defines window size.  code dependent on window size
 #pulls the dimensions from this variable
-gameWindow = (1,1)
+gameWindow = (100,100)
 #NOTE! This will need to likely be dynamic based on the size of the map
 #so this value should be overriden in gameEngine.__init__(), taking the
 #dimensions of the given map into account
@@ -48,9 +49,14 @@ class gameEngine():
 			if tempPos[1] > maxY:
 				maxY = tempPos[1]
 
-		gameWindow = (int(2*spriteR*(maxX+1)),int(2*spriteR*(maxY+1)))
-		#gameWindow = ((maxX+1),(maxY+1))
-		screen = pygame.display.set_mode(gameWindow)
+		#gameWindow = (int(2*spriteR*(maxX+1)),int(2*spriteR*(maxY+1)))
+		gameWindow = ((maxX+1),(maxY+1))
+		print(gameWindow)
+		winsize= [0,0] #this is the dimensions of the onscreen window
+		#gameWindow holds the dimensions of the game map itself in 1x1 squares
+		winsize[0] = int(gameWindow[0]*2*spriteR)
+		winsize[1] = int(gameWindow[1]*2*spriteR)
+		screen = pygame.display.set_mode(winsize)
 		pygame.display.flip()
 		pygame.display.set_caption('Freeze Tag')
 		
@@ -69,7 +75,7 @@ class gameEngine():
 			agentGroup.update(screen) #runs agent.update() on each agent in group
 			#agents themselves should check for if tagged, etc
 			wallGroup.update(screen)
-			time.sleep(0.01)#to slow it down
+			time.sleep(0.1)#to slow it down
 			pygame.display.flip()
 		
 		
@@ -84,7 +90,7 @@ class testAgent(pygame.sprite.Sprite):
 		self.hunter = 1
 		#we won't use this- ultimately will be pulling from agents.py
 		if self.agentPos == None:
-			self.agentPos = [random.randrange(40,gameWindow[0],40),random.randrange(40,gameWindow[1],40)]
+			self.agentPos = [random.randrange(3,gameWindow[0]-3,1),random.randrange(3,gameWindow[1]-3,1)]
 		self.rect = pygame.Rect(self.agentPos[0],self.agentPos[1], spriteR, spriteR)
 		if self.image == None:
 			if not random.randrange(0,5,1):
@@ -102,8 +108,7 @@ class testAgent(pygame.sprite.Sprite):
 		agentPos = self.agentPos
 		self.algorithm()	
 
-		#pygame.draw.circle(screen, self.thiscolor, agentPos, 15, 1)
-		self.rect = pygame.Rect(self.agentPos[0],self.agentPos[1], spriteR, spriteR)
+		self.rect = pygame.Rect(self.agentPos[0]*2*spriteR,self.agentPos[1]*2*spriteR, spriteR, spriteR)
 		screen.blit(self.image,self.rect)
 		#screen.blit() is what actually draws the image to the screen.
 		#need to update the rect. with current coordinates before drawing
@@ -116,33 +121,38 @@ class testAgent(pygame.sprite.Sprite):
 			#random code
 			x = (random.randrange(0,3,1)-1)
 			y = (random.randrange(0,3,1)-1)
-			agentPos[0] += spriteR*x
-			agentPos[1] += spriteR*y
+			agentPos[0] += x
+			agentPos[1] += y
 			if not self.validMove(wallList):#undo move if collides with a wall
-				agentPos[0] -= spriteR*x
-				agentPos[1] -= spriteR*y
+				agentPos[0] -= x
+				agentPos[1] -= y
+				None
 			self.agentPos = agentPos
 			#out of bounds collision.  real Agents should keep this from happening
 			#by not taking invalid moves offscreen
-			if agentPos[0] < 0:
-				agentPos[0] = 0
-			if agentPos[1] < 0:
-				agentPos[1] = 0
-			if agentPos[0] > (gameWindow[0]-2*spriteR):
-				agentPos[0] = (gameWindow[0]-2*spriteR)
-			if agentPos[1] > (gameWindow[1]-2*spriteR):
-				agentPos[1] = (gameWindow[1]-2*spriteR)
+			if agentPos[0] < 1:
+				agentPos[0] = 1
+			if agentPos[1] < 1:
+				agentPos[1] = 1
+			if agentPos[0] > (gameWindow[0]-2):
+				agentPos[0] = (gameWindow[0]-2)
+			if agentPos[1] > (gameWindow[1]-2):
+				agentPos[1] = (gameWindow[1]-2)
 		else:
 			#use algorithm
 			None
 
 
 	def validMove(self,wallList):
+		#this function, in this case, only checks if the proposed move is
+		#inside a wall.  More advanced agents may use a more elaborate function
+		p = self.agentPos
 		for i in wallList:
-			if pygame.sprite.collide_rect(self,i):
+			j = i.getPos()
+			if (j[0] == p[0]) and (j[1] == p[1]):
+				#print("Collision")
 				return False
 		return True
-	
 	
 	def getposition(self):
 		return agentPos
@@ -185,9 +195,8 @@ class tempGetMap():
 		
 	def getWalls(self):
 		return self.walls
-		
-		
-		
+
+
 		
 class wallTile(pygame.sprite.Sprite):
 	def __init__(self,_current_pos):
@@ -216,12 +225,11 @@ class wallTile(pygame.sprite.Sprite):
 #loop should iterate each agent one step, then redraw screen
 #code below is a basic implementation
 
-
 if __name__ == "__main__":
 	agentList = []
 	wallList = []
 	tempWalls = tempGetMap()
-	for i in range(1,200):
+	for i in range(1,50):
 		agentList.append(testAgent())
 	for i in tempWalls.getWalls():
 		#print(i)
